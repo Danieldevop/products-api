d35dc3b6da7d8099b06873fd2352786b84800bc0
Daniel <daniel.sendmemail@gmail>
2019-01-04 08:47:02 -0500

    Resquest and responses code updated

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 urlpatterns = [
 	
 	path('products', views.products_view),
 	path('products/<int:pk>', views.detail_product_view),
 
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- products/views.py
+++ products/views.py
@@ -1,57 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from .models import Product
 from .serializers import ProductSerializer
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 import pdb
 
-@csrf_exempt
-def products_view(request):
+@api_view(['GET', 'POST'])
+def products_view(request, format=None):
 
 	if request.method == 'GET':
 		product_query = Product.objects.all()
 		response = ProductSerializer(product_query, many=True)
-		return JsonResponse(response.data, safe=False)
+		return Response(response.data)
 
 	if request.method == 'POST':
-		product_request = JSONParser().parse(request)
-		product_serializer = ProductSerializer(data=product_request)
+		product_serializer = ProductSerializer(data=request.data)
 
 		if product_serializer.is_valid():
 			product_serializer.save()
-
-			return JsonResponse(product_serializer.data, status=201)
+			return Response(product_serializer.data, status=status.HTTP_201_CREATED)
 		
-		return JsonResponse(product_serializer.errors, status=400)
+		return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def detail_product_view(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def detail_product_view(request, pk, format=None):
 	#pdb.set_trace()
 	try:
 		detailed_product_query = Product.objects.get(pk=pk)
 	except Product.DoesNotExist:
-		return HttpResponse(status=404)
+		return Response(status=status.HTTP_400_NOT_FOUND)
 
 	if request.method == 'GET':
 		response = ProductSerializer(detailed_product_query)
-		return JsonResponse(response.data)
+		return Response(response.data)
 
 	if request.method == 'PUT':
-		data = JSONParser().parse(request)
-		serializer = ProductSerializer(detailed_product_query, data=data)
+		serializer = ProductSerializer(detailed_product_query, data=request.data)
 
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data)
+			return Response(serializer.data)
 
-		return JsonResponse(serializer.errors, status=400)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	if request.method == 'DELETE':
 		detailed_product_query.delete()
-		return HttpResponse(status=201)
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 		
